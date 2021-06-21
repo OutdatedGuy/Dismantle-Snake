ddc17d9530adfa4b8ae7bf9db0b715b29156a9e3
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-06-22 05:09:45 +0530

    Preloaded food images

--- assets/JS/sketch.js
+++ assets/JS/sketch.js
@@ -1,4 +1,13 @@
-let snake = [];
+let snake;
+let food;
+let catagory = [];
+
+function preload() {
+	let path = "./assets/Images/";
+	catagory[0] = loadImage(path + "Frog.png");
+	catagory[1] = loadImage(path + "disassemble.png");
+	catagory[2] = loadImage(path + "assemble.png");
+}
 
 function setup() {
 	const canvas = createCanvas(980, 540);
@@ -7,22 +16,31 @@ function setup() {
 		(window.innerHeight - height) / 2
 	);
 
-	snake[0] = new Snake((width - 20) / 2, (height - 20) / 2);
 	frameRate(15);
+
+	let size = 20;
+	snake = new Snake(width, height, (width - 20) / 2, (height - 20) / 2, size);
+
+	food = new Food(catagory, snake);
 }
 
 function draw() {
 	background(60);
-	snake[0].move();
-	snake[0].display("#0F0");
+
+	food.display(catagory);
+
+	if (!snake.move()) gameOver();
+	if (snake.eat(food)) food.newLocation(snake);
 }
 
 function keyPressed() {
-	if (keyCode === UP_ARROW && snake[0].speedY == 0) snake[0].update(0, -1);
-	else if (keyCode === RIGHT_ARROW && snake[0].speedX == 0)
-		snake[0].update(1, 0);
-	else if (keyCode === DOWN_ARROW && snake[0].speedY == 0)
-		snake[0].update(0, 1);
-	else if (keyCode === LEFT_ARROW && snake[0].speedX == 0)
-		snake[0].update(-1, 0);
+	if (keyCode === UP_ARROW && snake.speedY == 0) snake.update(0, -1);
+	else if (keyCode === RIGHT_ARROW && snake.speedX == 0) snake.update(1, 0);
+	else if (keyCode === DOWN_ARROW && snake.speedY == 0) snake.update(0, 1);
+	else if (keyCode === LEFT_ARROW && snake.speedX == 0) snake.update(-1, 0);
+}
+
+function gameOver() {
+	background(255, 0, 0);
+	noLoop();
 }
