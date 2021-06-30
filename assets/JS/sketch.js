2273089b1b8903742897ab1f58e766d75bd8db54
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-07-01 03:17:03 +0530

    Added full touch functionality

--- assets/JS/sketch.js
+++ assets/JS/sketch.js
@@ -120,4 +120,105 @@
 		isPlaying = true;
 		food = new Food(snake, document.getElementById("food"));
 	}
+
+	/**
+	 * Mobile swipping mode
+	 */
+	var supportTouch = $.support.touch,
+		scrollEvent = "touchmove scroll",
+		touchStartEvent = supportTouch ? "touchstart" : "mousedown",
+		touchStopEvent = supportTouch ? "touchend" : "mouseup",
+		touchMoveEvent = supportTouch ? "touchmove" : "mousemove";
+	$.event.special.swipeupdown = {
+		setup: function () {
+			var thisObject = this;
+			var $this = $(thisObject);
+			$this.bind(touchStartEvent, function (event) {
+				var data = event.originalEvent.touches
+						? event.originalEvent.touches[0]
+						: event,
+					start = {
+						time: new Date().getTime(),
+						coords: [data.pageX, data.pageY],
+						origin: $(event.target),
+					},
+					stop;
+
+				function moveHandler(event) {
+					if (!start) {
+						return;
+					}
+					var data = event.originalEvent.touches
+						? event.originalEvent.touches[0]
+						: event;
+					stop = {
+						time: new Date().getTime(),
+						coords: [data.pageX, data.pageY],
+					};
+
+					// prevent scrolling
+					event.preventDefault();
+				}
+				$this
+					.bind(touchMoveEvent, moveHandler)
+					.one(touchStopEvent, function (event) {
+						$this.unbind(touchMoveEvent, moveHandler);
+						if (start && stop) {
+							if (
+								stop.time - start.time < 1000 &&
+								Math.abs(start.coords[1] - stop.coords[1]) >
+									30 &&
+								Math.abs(start.coords[0] - stop.coords[0]) < 75
+							) {
+								start.origin
+									.trigger("swipeupdown")
+									.trigger(
+										start.coords[1] > stop.coords[1]
+											? "swipeup"
+											: "swipedown"
+									);
+							}
+						}
+						start = stop = undefined;
+					});
+			});
+		},
+	};
+	$.each(
+		{
+			swipedown: "swipeupdown",
+			swipeup: "swipeupdown",
+		},
+		function (event, sourceEvent) {
+			$.event.special[event] = {
+				setup: function () {
+					$(this).bind(sourceEvent, $.noop);
+				},
+			};
+		}
+	);
+
+	$("body").on("swipeup", function () {
+		if (snake.speedY == 0) swipped(0, -1);
+	});
+	$("body").on("swiperight", () => {
+		if (snake.speedX == 0) swipped(1, 0);
+	});
+	$("body").on("swipedown", function () {
+		if (snake.speedY == 0) swipped(0, 1);
+	});
+	$("body").on("swipeleft", () => {
+		if (snake.speedX == 0) swipped(-1, 0);
+	});
+
+	function swipped(xS, yS) {
+		if (isPlaying) {
+			if (snake.moved) snake.update(xS, yS);
+			else {
+				setTimeout(() => {
+					snake.update(xS, yS);
+				}, 69);
+			}
+		}
+	}
 }
